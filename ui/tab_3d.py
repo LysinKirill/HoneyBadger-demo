76d66180dff40f69294d176aca4e0f1687d08da1
Kirill Lysin <lysinkirill@gmail.com>
2026-02-22 23:04:02 +0300

    add sliders for multidimensional functions in 3d

--- ui/tab_3d.py
+++ ui/tab_3d.py
@@ -1,17 +1,86 @@
 from datetime import datetime
-from pathlib import Path
 
 import numpy as np
 from PyQt6.QtWidgets import (QWidget, QVBoxLayout, QHBoxLayout,
                              QPushButton, QLabel, QSpinBox, QSlider,
-                             QGroupBox, QComboBox, QGridLayout, QSplitter)
+                             QGroupBox, QComboBox, QGridLayout, QSplitter,
+                             QCheckBox, QLineEdit, QScrollArea)
 from PyQt6.QtCore import Qt, QTimer
 from PyQt6.QtGui import QFont
 import pyqtgraph.opengl as gl
 
 from core.export_data import DataExporter
-from core.functions import TEST_FUNCTIONS_3D
+from core.functions import TEST_FUNCTIONS_2D, TEST_FUNCTIONS_3D, TEST_FUNCTIONS_5D
 from core.honey_badger import HoneyBadgerAlgorithm, HBAParams
+
+
+class VariableSlider3D(QWidget):
+    def __init__(self, index, name, min_val, max_val, initial_val, parent_tab):
+        super().__init__()
+        self.index = index
+        self.name = name
+        self.min_val = min_val
+        self.max_val = max_val
+        self.parent_tab = parent_tab
+        self.updating = False
+
+        layout = QHBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        self.checkbox = QCheckBox(f"x{index + 1}")
+        self.checkbox.stateChanged.connect(self.on_checkbox_changed)
+        layout.addWidget(self.checkbox)
+
+        self.slider = QSlider(Qt.Orientation.Horizontal)
+        self.slider.setRange(0, 1000)
+        self.slider.setValue(int((initial_val - min_val) / (max_val - min_val) * 1000))
+        self.slider.valueChanged.connect(self.on_slider_changed)
+        layout.addWidget(self.slider)
+
+        self.textbox = QLineEdit()
+        self.textbox.setText(f"{initial_val:.3f}")
+        self.textbox.setFixedWidth(70)
+        self.textbox.textChanged.connect(self.on_text_changed)
+        layout.addWidget(self.textbox)
+
+        self.setLayout(layout)
+        self.set_active(True)
+
+    def on_checkbox_changed(self, state):
+        if not self.updating:
+            self.parent_tab.update_selected_variables()
+
+    def on_slider_changed(self, value):
+        if self.updating:
+            return
+
+        self.updating = True
+        val = self.min_val + (value / 1000.0) * (self.max_val - self.min_val)
+        self.textbox.setText(f"{val:.3f}")
+        self.updating = False
+        self.parent_tab.on_variable_changed()
+
+    def on_text_changed(self, text):
+        if self.updating:
+            return
+
+        try:
+            val = float(text)
+            if self.min_val <= val <= self.max_val:
+                self.updating = True
+                slider_val = int((val - self.min_val) / (self.max_val - self.min_val) * 1000)
+                self.slider.setValue(slider_val)
+                self.updating = False
+                self.parent_tab.on_variable_changed()
+        except ValueError:
+            pass
+
+    def get_value(self):
+        return float(self.textbox.text())
+
+    def set_active(self, active):
+        self.slider.setEnabled(active)
+        self.textbox.setEnabled(active)
 
 
 class Tab3D(QWidget):
@@ -21,9 +90,13 @@ class Tab3D(QWidget):
         self.bounds = None
         self.optimum = None
         self.func_name = None
+        self.func_dim = 3
         self.hba = None
         self.is_animating = False
         self.trajectory_history = []
+        self.variable_sliders = []
+        self.fixed_values = []
+        self.selected_vars = [0, 1, 2]
 
         self.init_ui()
         self.setup_default_function()
@@ -36,6 +109,9 @@ class Tab3D(QWidget):
 
         control_panel = self.create_control_panel()
         main_layout.addWidget(control_panel)
+
+        self.var_panel = self.create_variable_panel()
+        main_layout.addWidget(self.var_panel)
 
         splitter = QSplitter(Qt.Orientation.Horizontal)
 
@@ -58,7 +134,9 @@ class Tab3D(QWidget):
 
         layout.addWidget(QLabel("Function:"), 0, 0)
         self.func_combo = QComboBox()
-        self.func_combo.addItems(TEST_FUNCTIONS_3D.keys())
+
+        all_functions = {**TEST_FUNCTIONS_2D, **TEST_FUNCTIONS_3D, **TEST_FUNCTIONS_5D}
+        self.func_combo.addItems(all_functions.keys())
         self.func_combo.currentTextChanged.connect(self.change_function)
         layout.addWidget(self.func_combo, 0, 1)
 
@@ -108,6 +186,22 @@ class Tab3D(QWidget):
         panel.setLayout(layout)
         return panel
 
+    def create_variable_panel(self):
+        panel = QGroupBox("Variable Controls (Select 3 variables for 3D plot)")
+        layout = QVBoxLayout()
+
+        scroll = QScrollArea()
+        scroll.setWidgetResizable(True)
+        scroll.setMaximumHeight(150)
+
+        scroll_widget = QWidget()
+        self.var_layout = QVBoxLayout(scroll_widget)
+        scroll.setWidget(scroll_widget)
+
+        layout.addWidget(scroll)
+        panel.setLayout(layout)
+        return panel
+
     def create_info_panel(self):
         panel = QWidget()
         layout = QVBoxLayout(panel)
@@ -149,34 +243,175 @@ class Tab3D(QWidget):
         cam_group.setLayout(cam_layout)
         layout.addWidget(cam_group)
 
+        cursor_group = QGroupBox("Current Constants")
+        cursor_layout = QVBoxLayout()
+        self.constants_label = QLabel("Fixed variables: None")
+        self.constants_label.setWordWrap(True)
+        cursor_layout.addWidget(self.constants_label)
+        cursor_group.setLayout(cursor_layout)
+        layout.addWidget(cursor_group)
+
         layout.addStretch()
         return panel
+
+    def setup_variable_controls(self):
+        for slider in self.variable_sliders:
+            slider.setParent(None)
+            slider.deleteLater()
+        self.variable_sliders.clear()
+
+        while self.var_layout.count():
+            item = self.var_layout.takeAt(0)
+            if item.widget():
+                item.widget().deleteLater()
+
+        if "5D" in self.func_name:
+            self.func_dim = 5
+        elif "3D" in self.func_name:
+            self.func_dim = 3
+        else:
+            self.func_dim = 2
+        bounds_list = [self.bounds] * self.func_dim
+
+        self.fixed_values = [0.0] * self.func_dim
+
+        for i in range(self.func_dim):
+            min_val = bounds_list[i][0]
+            max_val = bounds_list[i][1]
+            initial = (min_val + max_val) / 2
+
+            slider = VariableSlider3D(i, f"x{i + 1}", min_val, max_val, initial, self)
+            self.var_layout.addWidget(slider)
+            self.variable_sliders.append(slider)
+            self.fixed_values[i] = initial
+
+        num_to_select = min(3, len(self.variable_sliders))
+        for i in range(num_to_select):
+            self.variable_sliders[i].checkbox.setChecked(True)
+
+    def update_selected_variables(self):
+        selected = []
+        for i, slider in enumerate(self.variable_sliders):
+            if slider.checkbox.isChecked():
+                selected.append(i)
+
+        if len(selected) > 3:
+            for i in range(len(self.variable_sliders) - 1, -1, -1):
+                if self.variable_sliders[i].checkbox.isChecked():
+                    self.variable_sliders[i].checkbox.setChecked(False)
+                    break
+
+        self.selected_vars = []
+        for i, slider in enumerate(self.variable_sliders):
+            if slider.checkbox.isChecked():
+                self.selected_vars.append(i)
+                slider.set_active(False)
+            else:
+                slider.set_active(True)
+
+        if len(self.selected_vars) < 3:
+            needed = 3 - len(self.selected_vars)
+            for i in range(len(self.variable_sliders)):
+                if i not in self.selected_vars and needed > 0:
+                    self.variable_sliders[i].checkbox.setChecked(True)
+                    self.selected_vars.append(i)
+                    self.variable_sliders[i].set_active(False)
+                    needed -= 1
+
+        self.update_constants_display()
+        self.on_variable_changed()
+
+    def update_constants_display(self):
+        fixed_vars = []
+        for i, slider in enumerate(self.variable_sliders):
+            if not slider.checkbox.isChecked():
+                fixed_vars.append(f"x{i + 1}={slider.get_value():.3f}")
+
+        if fixed_vars:
+            self.constants_label.setText("Fixed: " + ", ".join(fixed_vars))
+        else:
+            self.constants_label.setText("Fixed variables: None")
+
+    def on_variable_changed(self):
+        for i, slider in enumerate(self.variable_sliders):
+            if not slider.checkbox.isChecked():
+                self.fixed_values[i] = slider.get_value()
+            else:
+                pass
+
+        self.update_constants_display()
+        self.plot_function_surface()
+
+    def create_reduced_function(self):
+        def reduced_func(x, y, z):
+            full_input = self.fixed_values.copy()
+            while len(full_input) < self.func_dim:
+                full_input.append(0)
+            full_input = full_input[:self.func_dim]
+            if len(self.selected_vars) >= 3:
+                full_input[self.selected_vars[0]] = x
+                full_input[self.selected_vars[1]] = y
+                full_input[self.selected_vars[2]] = z
+            elif len(self.selected_vars) == 2:
+                full_input[self.selected_vars[0]] = x
+                full_input[self.selected_vars[1]] = y
+            elif len(self.selected_vars) == 1:
+                full_input[self.selected_vars[0]] = x
+
+            return self.func(np.array(full_input))
+
+        return reduced_func
 
     def setup_default_function(self):
         self.func_name = list(TEST_FUNCTIONS_3D.keys())[0]
         self.func, self.bounds, self.optimum = TEST_FUNCTIONS_3D[self.func_name]
+        self.setup_variable_controls()
         self.plot_function_surface()
 
     def change_function(self, func_name):
         self.func_name = func_name
-        self.func, self.bounds, self.optimum = TEST_FUNCTIONS_3D[func_name]
+
+        if func_name in TEST_FUNCTIONS_2D:
+            self.func, self.bounds, self.optimum = TEST_FUNCTIONS_2D[func_name]
+        elif func_name in TEST_FUNCTIONS_3D:
+            self.func, self.bounds, self.optimum = TEST_FUNCTIONS_3D[func_name]
+        else:
+            self.func, self.bounds, self.optimum = TEST_FUNCTIONS_5D[func_name]
+
+        self.setup_variable_controls()
         self.plot_function_surface()
         self.reset()
 
     def plot_function_surface(self):
         self.gl_widget.clear()
 
-        resolution = 50
-        x = np.linspace(self.bounds[0], self.bounds[1], resolution)
-        y = np.linspace(self.bounds[0], self.bounds[1], resolution)
+        if len(self.selected_vars) < 3:
+            text = gl.GLTextItem(pos=(0, 0, 0), text="Select 3 variables for 3D plot")
+            self.gl_widget.addItem(text)
+            return
+
+        plot_func = self.create_reduced_function()
+
+        x_min = self.variable_sliders[self.selected_vars[0]].min_val
+        x_max = self.variable_sliders[self.selected_vars[0]].max_val
+        y_min = self.variable_sliders[self.selected_vars[1]].min_val
+        y_max = self.variable_sliders[self.selected_vars[1]].max_val
+        z_min = self.variable_sliders[self.selected_vars[2]].min_val
+        z_max = self.variable_sliders[self.selected_vars[2]].max_val
+        fixed_z = self.fixed_values[self.selected_vars[2]]
+
+        resolution = 30
+        x = np.linspace(x_min, x_max, resolution)
+        y = np.linspace(y_min, y_max, resolution)
         X, Y = np.meshgrid(x, y)
 
         Z = np.zeros_like(X)
         for i in range(resolution):
             for j in range(resolution):
-                Z[i, j] = self.func(np.array([X[i, j], Y[i, j], 0]))
+                Z[i, j] = plot_func(X[i, j], Y[i, j], fixed_z)
 
-        z_min, z_max = Z.min(), Z.max()
+        z_vals = Z.flatten()
+        z_min_val, z_max_val = z_vals.min(), z_vals.max()
 
         verts = []
         faces = []
@@ -192,25 +427,47 @@ class Tab3D(QWidget):
                 idx = len(verts)
                 verts.extend([p1, p2, p3, p4])
 
-                c = (0.2 + 0.8 * (Z[i, j] - z_min) / (z_max - z_min) if z_max > z_min else 0.6,
-                     0.5 - 0.5 * (Z[i, j] - z_min) / (z_max - z_min) if z_max > z_min else 0.4,
-                     1.0 - 0.8 * (Z[i, j] - z_min) / (z_max - z_min) if z_max > z_min else 0.2,
-                     1.0)
+                if z_max_val > z_min_val:
+                    norm_z = (Z[i, j] - z_min_val) / (z_max_val - z_min_val)
+                else:
+                    norm_z = 0.5
+
+                c = (norm_z, 0.5, 1.0 - norm_z, 1.0)
                 colors.extend([c, c, c, c])
 
                 faces.append([idx, idx + 1, idx + 2])
                 faces.append([idx + 1, idx + 3, idx + 2])
 
-        mesh = gl.GLMeshItem(
-            vertexes=np.array(verts),
-            faces=np.array(faces),
-            vertexColors=np.array(colors),
-            smooth=True,
-            drawEdges=False,
-            drawFaces=True,
-            shader='shaded'
+        if verts:
+            mesh = gl.GLMeshItem(
+                vertexes=np.array(verts),
+                faces=np.array(faces),
+                vertexColors=np.array(colors),
+                smooth=True,
+                drawEdges=False,
+                drawFaces=True,
+                shader='shaded'
+            )
+            self.gl_widget.addItem(mesh)
+
+        axis_length = max(x_max - x_min, y_max - y_min, z_max - z_min) * 0.3
+        x_axis = gl.GLLinePlotItem(
+            pos=np.array([[-axis_length, 0, 0], [axis_length, 0, 0]]),
+            color=(1, 0, 0, 1), width=2
         )
-        self.gl_widget.addItem(mesh)
+        self.gl_widget.addItem(x_axis)
+
+        y_axis = gl.GLLinePlotItem(
+            pos=np.array([[0, -axis_length, 0], [0, axis_length, 0]]),
+            color=(0, 1, 0, 1), width=2
+        )
+        self.gl_widget.addItem(y_axis)
+
+        z_axis = gl.GLLinePlotItem(
+            pos=np.array([[0, 0, -axis_length], [0, 0, axis_length]]),
+            color=(0, 0, 1, 1), width=2
+        )
+        self.gl_widget.addItem(z_axis)
 
         self.population_scatter = gl.GLScatterPlotItem(
             size=15, color=(1, 0, 0, 1), pxMode=True
@@ -222,13 +479,17 @@ class Tab3D(QWidget):
         )
         self.gl_widget.addItem(self.best_scatter)
 
+        self.gl_widget.addItem(gl.GLTextItem(pos=(axis_length * 1.2, 0, 0), text=f"x{self.selected_vars[0] + 1}"))
+        self.gl_widget.addItem(gl.GLTextItem(pos=(0, axis_length * 1.2, 0), text=f"x{self.selected_vars[1] + 1}"))
+        self.gl_widget.addItem(gl.GLTextItem(pos=(0, 0, axis_length * 1.2), text=f"x{self.selected_vars[2] + 1}"))
+
     def initialize(self):
         params = HBAParams(
             pop_size=self.spin_pop.value(),
             max_iter=self.spin_iter.value()
         )
         self.hba = HoneyBadgerAlgorithm(params)
-        self.hba.set_optimization_problem(self.func, 3, self.bounds)
+        self.hba.set_optimization_problem(self.func, self.func_dim, self.bounds)
 
         self.btn_step.setEnabled(True)
         self.btn_play.setEnabled(True)
@@ -257,34 +518,58 @@ class Tab3D(QWidget):
         self.update_display()
 
     def update_3d_plot(self):
-        if self.hba.population is None:
+        if self.hba is None or self.hba.population is None or len(self.selected_vars) < 3:
             return
 
-        pop = self.hba.population
-        fitness = np.array([self.func(ind) for ind in pop])
+        x = self.hba.population[:, self.selected_vars[0]]
+        y = self.hba.population[:, self.selected_vars[1]]
+        z = self.hba.population[:, self.selected_vars[2]]
 
-        pos = np.zeros((pop.shape[0], 3))
-        pos[:, :2] = pop[:, :2]
-        pos[:, 2] = fitness
-
+        pos = np.column_stack([x, y, z])
         self.population_scatter.setData(pos=pos)
 
         if self.hba.best_solution is not None:
-            best = self.hba.best_solution
-            best_pos = np.zeros((1, 3))
-            best_pos[0, :2] = best[:2]
-            best_pos[0, 2] = self.func(best)
+            best_pos = np.array([[
+                self.hba.best_solution[self.selected_vars[0]],
+                self.hba.best_solution[self.selected_vars[1]],
+                self.hba.best_solution[self.selected_vars[2]]
+            ]])
             self.best_scatter.setData(pos=best_pos)
+
+        if hasattr(self.hba, 'previous_population') and self.hba.previous_population is not None:
+            for lines in self.trajectory_history:
+                for line in lines:
+                    self.gl_widget.removeItem(line)
+            self.trajectory_history = []
+
+            new_lines = []
+            for i in range(len(x)):
+                x_old = self.hba.previous_population[i, self.selected_vars[0]]
+                y_old = self.hba.previous_population[i, self.selected_vars[1]]
+                z_old = self.hba.previous_population[i, self.selected_vars[2]]
+
+                if (abs(x[i] - x_old) > 0.01 or
+                        abs(y[i] - y_old) > 0.01 or
+                        abs(z[i] - z_old) > 0.01):
+                    line = gl.GLLinePlotItem(
+                        pos=np.array([x_old, y_old, z_old], [x[i], y[i], z[i]]),
+                        color=(0, 0, 1, 0.3),
+                        width=1,
+                        antialias=True
+                    )
+                    self.gl_widget.addItem(line)
+                    new_lines.append(line)
+
+                    if new_lines:
+                        self.trajectory_history.append(new_lines)
 
     def update_display(self):
         self.iter_label.setText(f"Iteration: {self.hba.current_iter}/{self.hba.params.max_iter}")
         self.best_label.setText(f"Best Fitness: {self.hba.best_fitness:.6f}")
 
         if self.hba.best_solution is not None:
-            sol = self.hba.best_solution
-            self.solution_label.setText(
-                f"Best Solution: [{sol[0]:.4f}, {sol[1]:.4f}, {sol[2]:.4f}]"
-            )
+            sol_str = ", ".join([f"{v:.4f}" for v in self.hba.best_solution[:5]])
+            self.solution_label.setText(f"Best Solution: [{sol_str}]")
 
         phase = getattr(self.hba, 'current_phase', 'Unknown')
         self.phase_label.setText(f"Phase: {phase}")
@@ -343,7 +628,9 @@ class Tab3D(QWidget):
             'bounds': self.bounds,
             'optimum': self.optimum.tolist(),
             'optimization_results': self.hba.get_optimization_history(),
-            'timestamp': datetime.now().isoformat()
+            'timestamp': datetime.now().isoformat(),
+            'selected_variables': self.selected_vars,
+            'fixed_values': self.fixed_values
         }
 
         filename = f"{self.func_name.replace(' ', '_')}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
